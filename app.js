74a9c9844d6597dcc67068f2a2b7e5de0030af80
Leandro Blandi <59542421+leandroblandi@users.noreply.github.com>
2023-10-10 22:38:24 -0300

    Add files via upload
    update taxes, added support container, etc.

--- app.js
+++ app.js
@@ -1,66 +1,69 @@
-
-function calculateBill()
-{
+function calculateTax() {
+  const taxAmount = 2;
   const costValue = document.querySelector(".costEntry");
-  let newCostValue = costValue.value.replace(/,/g, '.');
-  return newCostValue * 1.75;
+  let newCostValue = costValue.value.replace(/,/g, ".");
+
+  return newCostValue * taxAmount;
 }
 
-function showBill()
-{
+function showTax() {
   const containerToShow = document.querySelector(".costResult");
-  const cost = calculateBill();
+  const cost = calculateTax();
 
-  if(cost < 0)
-  {
+  if (cost < 0) {
     swal("Error :(", "No hay precios negativos, fijate bien", "error");
     return;
   }
 
-  if(cost === 0) {
+  if (cost === 0) {
     swal("Error :(", "Al menos tenés que ingresar un precio", "error");
     return;
   }
 
-  if(isNaN(cost))
-  {
-    swal("Cuidado :O", "Al parecer no ingresaste un precio válido para la App", "warning");
+  if (isNaN(cost)) {
+    swal(
+      "Cuidado :O",
+      "Al parecer no ingresaste un precio válido para la App",
+      "warning"
+    );
     return;
   }
 
-  if((cost > 0) && (cost < 500)) {
+  if (cost > 0 && cost < 500) {
     let cheaperEmoji = document.createElement("img");
     cheaperEmoji.src = "./static/blushed_smile.png";
     cheaperEmoji.width = 24;
     cheaperEmoji.height = 24;
 
-    containerToShow.innerHTML = "Te costará ARS<span class='cost cheaper'>&nbsp;$" + parseFloat(cost) + "<a href='#estimatedCostMessage'>(*)</a></span>";
+    containerToShow.innerHTML =
+      "Te costará ARS<span class='cost cheaper'>&nbsp;$" +
+      parseFloat(cost) +
+      "<a href='#estimatedCostMessage'>(*)</a></span>";
     containerToShow.appendChild(cheaperEmoji);
-  }
-
-  else if((cost >= 500) && (cost < 2000)) {
+  } else if (cost >= 500 && cost < 2000) {
     let mediumCostEmoji = document.createElement("img");
     mediumCostEmoji.src = "./static/smile.png";
     mediumCostEmoji.width = 24;
     mediumCostEmoji.height = 24;
 
-    containerToShow.innerHTML = "Te costará ARS<span class='cost mediumCost'>&nbsp;$" + parseFloat(cost) + "<a href='#estimatedCostMessage'>(*)</a></span>";
+    containerToShow.innerHTML = `Te costará ARS<span class='cost mediumCost'>&nbsp;$${parseFloat(
+      cost
+    )} <a href='#estimatedCostMessage'>(*)</a></span>`;
     containerToShow.appendChild(mediumCostEmoji);
-  }
-
-  else if((cost >= 2000)) {
+  } else if (cost >= 2000) {
     let expensiveEmoji = document.createElement("img");
     expensiveEmoji.src = "./static/tearing_smile.png";
     expensiveEmoji.width = 24;
     expensiveEmoji.height = 24;
 
-    containerToShow.innerHTML = "Te costará ARS<span class='cost expensive'>&nbsp;$" + parseFloat(cost) + "<a class='estimatedCostAterisk' href='#estimatedCostMessage'>(*)</a></span>";
+    containerToShow.innerHTML =
+      "Te costará ARS<span class='cost expensive'>&nbsp;$" +
+      parseFloat(cost) +
+      "<a class='estimatedCostAterisk' href='#estimatedCostMessage'>(*)</a></span>";
     containerToShow.appendChild(expensiveEmoji);
   }
 
   // Mostramos el valor si ninguna condicion es prevista
-  
-   
 }
 
 function help() {
@@ -69,44 +72,30 @@ function help() {
     html: "Esta app es súper sencilla de utilizar. Primero que nada:<br><span class='separator'></span><div class='steps'><b>1.</b> Te dirigís a la página de Steam y mirás el precio del juego que desees comprar<br></div><div class='steps'><b>2.</b> Escribís el precio que viste en la entrada de esta página<br></div><div class='steps'><b>3.</b> Presionás en 'Calcular'</div><span class='separator'></span>¡Y listo! Ya te devuelve el precio final que estarías pagando",
     confirmButtonText: "Adelante",
     type: "info",
-    customClass: "helpAlert"
+    customClass: "helpAlert",
   });
 }
 
-document.addEventListener("DOMContentLoaded", ()=> {
-
+document.addEventListener("DOMContentLoaded", () => {
   const calculateInput = document.querySelector(".costEntry");
   const calculateButton = document.querySelector(".costButton");
   const helpButton = document.querySelector(".help__button");
-  //const estimatedCostAterisk = document.querySelector(".estimatedCostAterisk");
-  //const estimatedCostMessage = document.getElementById("estimatedCostMessage");
 
-
-  helpButton.addEventListener("click", (e)=> {
+  helpButton.addEventListener("click", (e) => {
     e.preventDefault();
     help();
   });
 
-  calculateInput.addEventListener("keydown", (event)=> {
-
-    if(event.keyCode === 13) { // Si presiona Enter
+  calculateInput.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+      // Si presiona Enter
       event.preventDefault();
-      showBill();
+      showTax();
     }
   });
 
-  calculateButton.addEventListener("click", (event)=> {
-
+  calculateButton.addEventListener("click", (event) => {
     event.preventDefault();
-    showBill();
+    showTax();
   });
-
-  //estimatedCostAterisk.addEventListener("click", (event)=> {
-    
-    //e.preventDefault();
-
-   // estimatedCostMessage.focus();
-
-  //});
 });
-
